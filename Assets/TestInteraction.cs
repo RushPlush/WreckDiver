69eecea531aec36b10e3bd844e5815f594c71368
AzeCeres <juliantokle@gmail.com>
2025-03-13 00:35:55 +0100

    Update TestInteraction.cs

--- Assets/TestInteraction.cs
+++ Assets/TestInteraction.cs
@@ -2,12 +2,32 @@ using UnityEngine;
 
 public class TestInteraction : MonoBehaviour, IInteractable
 {
+    private Outline m_Outline;
+    [SerializeField]private IInteractable.InteractionType interactionType;
+    
+    private void Awake()
+    {
+        m_Outline = GetComponent<Outline>();
+        m_Outline.enabled = false;
+    }
+
+    IInteractable.InteractionType IInteractable.interactionType
+    {
+        get => interactionType;
+        set => interactionType = value;
+    }
+
     public void Interact(GameObject player)
     {
         throw new System.NotImplementedException();
     }
 
-    public void Interact(GameObject player, GameObject item)
+    public void Interact(GameObject player, Item item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact(Item item)
     {
         throw new System.NotImplementedException();
     }
@@ -22,14 +42,20 @@ public class TestInteraction : MonoBehaviour, IInteractable
         throw new System.NotImplementedException();
     }
 
+    public void InteractionFailed()
+    {
+        throw new System.NotImplementedException();
+    }
+
     public void Highlight()
     {
         print(transform.name + " Highlighted");
+        m_Outline.enabled = true;
     }
 
     public void Unhighlight()
     {
-        throw new System.NotImplementedException();
+        m_Outline.enabled = false;
     }
 }
 
