8467779e7a99a08f6b808b31be36b0ad36b87842
Sebastian R Sletten <Sebaslett@gmail.com>
2025-04-23 13:40:13 +0200

    Flocking Fishes AI finished
    Flocking is now functional with multiple sets off spawners for different swarms of fish

--- Assets/Scripts/AI/Flock.cs
+++ Assets/Scripts/AI/Flock.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Flock : MonoBehaviour 
 {
     float speed;
+    bool turning = false;
     
     void Start ()
     {
@@ -14,7 +15,34 @@ public class Flock : MonoBehaviour
 
     void Update ()
     {
-        ApplyRules();
+        Bounds b = new Bounds(FlockManager.FM.transform.position, FlockManager.FM.swimLimits * 2);
+
+        if (!b.Contains(transform.position))
+        {
+            turning = true;
+        }
+        else
+            turning = false;
+
+        if (turning)
+        {
+            Vector3 direction = FlockManager.FM.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),
+                FlockManager.FM.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+            {
+                speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+            }
+
+            if (Random.Range(0, 100) < 10)
+            {
+                ApplyRules();
+            }
+        }
+
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 
@@ -52,8 +80,12 @@ public class Flock : MonoBehaviour
 
         if (groupSize > 0)
         {
-            vCenter = vCenter / groupSize;
+            vCenter = vCenter / groupSize + (FlockManager.FM.goalPos - this.transform.position);
             speed = gSpeed / groupSize;
+            if (speed > FlockManager.FM.maxSpeed)
+            {
+                speed = FlockManager.FM.maxSpeed;
+            }
 
             Vector3 direction = (vCenter + vAvoid) - transform.position;
             if (direction != Vector3.zero) 
