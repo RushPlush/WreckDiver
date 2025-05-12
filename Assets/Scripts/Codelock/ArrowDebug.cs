812590ce63c56da72d2e1d88caa239a9249c2e1c
wolfcomp <4028289+wolfcomp@users.noreply.github.com>
2025-05-12 12:30:54 +0200

    fix some underlying code issues

---
+++ Assets/Scripts/Codelock/ArrowDebug.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class ArrowDebug : MonoBehaviour
+{
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(transform.forward), 0.1f, EventType.Repaint);
+        Handles.ArrowHandleCap(0, transform.position, Quaternion.LookRotation(transform.up), 0.1f, EventType.Repaint);
+    }
+    #endif
+}
